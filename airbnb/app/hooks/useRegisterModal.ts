bc28acaa6c912df0ba8c1630f0bd024abae7cc43
hoang <hoangkrt1@gmail.com>
2023-08-03 18:11:39 +0700

    xong set up db

--- airbnb/app/hooks/useRegisterModal.ts
+++ airbnb/app/hooks/useRegisterModal.ts
@@ -1,15 +1,16 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
-interface ResgisterModalStore{
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
+interface RegisterModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
-const useRegisterModal = create <ResgisterModalStore> ((set) => ({
-    isOpen: false,
-     onOpen: () => set({isOpen: true}),
-     onClose: () => set({isOpen: false})
+const useRegisterModal = create<RegisterModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false })
 }));
+
 
 export default useRegisterModal;
