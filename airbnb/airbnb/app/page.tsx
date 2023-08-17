f67d5fb79dcaeb0564e59e5470c81848d118903b
hoang <hoangkrt1@gmail.com>
2023-08-17 15:58:21 +0700

    FullProject

---
+++ airbnb/airbnb/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div className="text-rose-400 text-6xl">Wellcome to Airbnb</div>
+  )
+}
--- airbnb/app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="text-rose-400 text-6xl"></div>
-  )
-}
